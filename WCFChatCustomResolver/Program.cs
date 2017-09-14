eed939b15a53fa73af5aa8235115fce528f1ecba
Vinicius <Vinicius@DESKTOP-78J4IIV>
2017-09-13 23:29:44 -0300

    Custom resolver server.

---
+++ WCFChatCustomResolver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.PeerResolvers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFChatCustomResolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CustomPeerResolverService crs = new CustomPeerResolverService();
+            crs.ControlShape = false;
+
+            ServiceHost customResolver = new ServiceHost(crs);
+
+            
+            crs.Open();
+            customResolver.Open();
+            Console.WriteLine("Servidor custom resolver rodando");
+            Console.ReadLine();
+        }
+    }
+}
